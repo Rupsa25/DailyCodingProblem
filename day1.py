70bddaa3d82d3b27a20ad6b8a79ea13f3d8314d2
Rupsa <rupsa1671@gmail.com>
2020-12-15 14:08:54 +0530

    Add files via upload

---
+++ day1.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec 15 08:44:07 2020
+
+@author: Rupsa
+"""
+"""Time complexity O(nlogn)"""
+"""Given a list of numbers and a number k, return whether any two numbers from the list add up to k."""
+def sumExists(arr,k):
+    arr.sort()
+    i=0
+    j=len(arr)-1
+    while i<j:
+        if arr[i]+arr[j]==k:
+            return True
+        elif arr[i]+arr[j]>k:
+            j-=1
+        elif arr[i]+arr[j]<k:
+            i+=1
+    return False
